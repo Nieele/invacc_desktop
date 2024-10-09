0a61a62f72a79d43dfc342fd62f26f79ceb0471b
никита шумов <nikitashumov@gmail.com>
2024-10-09 07:42:23 +0300

    feat(LoginForm): add window drag

--- invacc/loginForm.cs
+++ invacc/loginForm.cs
@@ -4,17 +4,38 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace invacc
 {
-    public partial class frmLogin : Form
+    public partial class FrmLogin : Form
     {
-        public frmLogin()
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr LPAR);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        const int WM_NCLBUTTONDOWN = 0xA1;
+        const int HT_CAPTION = 0x2;
+
+        private void Move_window(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                _ = SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        public FrmLogin()
         {
             InitializeComponent();
+            this.MouseDown += new MouseEventHandler(Move_window);
+            lblNameProg.MouseDown += new MouseEventHandler(Move_window);
         }
     }
 }
