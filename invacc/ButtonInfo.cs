b922450a6f661e727c686a6a20578ddc4c3080c9
никита шумов <nikitashumov@gmail.com>
2024-10-14 23:45:44 +0300

    feat: add ButtonInfo.cs for generate buttons in panelSide

---
+++ invacc/ButtonInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invacc
+{
+    class ButtonInfo(string name, Action action)
+    {
+        public string Name { get; } = name;
+        public Action Action { get; } = action;
+    }
+}
