b9d002367cb4addeb066afc841e0a19beeed8404
nikita <nikitashumov@gmail.com>
2025-03-20 14:16:45 +0300

    feat(backend): add starting the server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"invacc-backend/internal/app"
 	"invacc-backend/internal/config"
+	"invacc-backend/pkg/db"
 	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -13,5 +17,15 @@ func main() {
 		panic("config is not load")
 	}
 
-	log.Println(cfg)
+	dbConn, err := db.GetDB(config.GetConnStringDB())
+	if err != nil {
+		log.Fatal("database connection error")
+	}
+
+	router := app.NewRouter(dbConn, cfg)
+
+	addr := cfg.HTTPServer.Address + ":" + strconv.Itoa(cfg.HTTPServer.Port)
+	log.Fatal(http.ListenAndServe(addr, router))
+
+	log.Println("server is up", cfg.HTTPServer.Address, ":", cfg.HTTPServer.Port)
 }
