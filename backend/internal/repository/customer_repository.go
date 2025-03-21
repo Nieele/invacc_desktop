2af3d6d5080c1f6544b63ab934bbfc6a13677a78
nikita <nikitashumov@gmail.com>
2025-03-21 10:33:58 +0300

    feat(backend/repository): add customer repo - get personal info, update personal info

---
+++ backend/internal/repository/customer_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"invacc-backend/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type CustomerRepository interface {
+	GetInfo(id int) (models.CustomerInfo, error)
+	UpdateInfo(info models.CustomerInfo) error
+}
+
+type customerRepo struct {
+	db *gorm.DB
+}
+
+func NewCustomerRepository(db *gorm.DB) CustomerRepository {
+	return &customerRepo{db: db}
+}
+
+func (r *customerRepo) GetInfo(id int) (models.CustomerInfo, error) {
+	var info models.CustomerInfo
+	result := r.db.Where("id = ?", id).First(&info)
+	if result.Error != nil {
+		return models.CustomerInfo{}, result.Error
+	}
+	return info, nil
+}
+
+func (r *customerRepo) UpdateInfo(info models.CustomerInfo) error {
+	if err := r.db.Save(&info).Error; err != nil {
+		return err
+	}
+	return nil
+}
