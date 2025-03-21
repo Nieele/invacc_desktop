0998cf4f6f8a60d5328df5da596c56242ed980d0
nikita <nikitashumov@gmail.com>
2025-03-21 12:15:00 +0300

    refactor(backend/item_service): remove unused functions

--- backend/internal/service/item_service.go
+++ backend/internal/service/item_service.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"encoding/json"
 	"invacc-backend/internal/models"
 	"invacc-backend/internal/repository"
 
@@ -9,8 +8,6 @@ import (
 )
 
 type ItemService interface {
-	GetItem(id int) ([]byte, error)
-	GetItemsList(page int) ([]byte, error)
 	GetItemModel(id int) (models.Item, error)
 	GetItemModelList(page int) ([]models.Item, error)
 }
@@ -22,22 +19,6 @@ type itemService struct {
 func NewItemService(db *gorm.DB) ItemService {
 	repo := repository.NewItemRepository(db)
 	return &itemService{itemRepo: repo}
-}
-
-func (s *itemService) GetItem(id int) ([]byte, error) {
-	item, err := s.itemRepo.GetItem(id)
-	if err != nil {
-		return nil, err
-	}
-	return json.Marshal(item)
-}
-
-func (s *itemService) GetItemsList(page int) ([]byte, error) {
-	items, err := s.itemRepo.GetItemsList(page)
-	if err != nil {
-		return nil, err
-	}
-	return json.Marshal(items)
 }
 
 func (s *itemService) GetItemModel(id int) (models.Item, error) {
