909e014bdbf606a824b809d27191a082da5393b4
nikita <nikitashumov@gmail.com>
2025-03-21 10:56:49 +0300

    feat(backend/service): add customer service - user info management

---
+++ backend/internal/service/customer_service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"errors"
+	"invacc-backend/internal/models"
+	"invacc-backend/internal/repository"
+
+	"gorm.io/gorm"
+)
+
+type CustomerService interface {
+	GetPesonalInfo(id int, token string) (models.CustomerInfo, error)
+	UpdatePersonalInfo(token string, info models.CustomerInfo) error
+}
+
+type customerService struct {
+	customerRepo repository.CustomerRepository
+	authService  AuthService
+}
+
+func NewCustomerService(db *gorm.DB, authService AuthService) CustomerService {
+	repo := repository.NewCustomerRepository(db)
+	return &customerService{customerRepo: repo, authService: authService}
+}
+
+func (s *customerService) GetPesonalInfo(id int, token string) (models.CustomerInfo, error) {
+	if err := s.authService.Authentication(token); err != nil {
+		return models.CustomerInfo{}, errors.New("access denied")
+	}
+	info, err := s.customerRepo.GetInfo(id)
+	if err != nil {
+		return models.CustomerInfo{}, err
+	}
+
+	return info, nil
+}
+
+func (s *customerService) UpdatePersonalInfo(token string, info models.CustomerInfo) error {
+	if err := s.authService.Authentication(token); err != nil {
+		return errors.New("access denied")
+	}
+
+	if err := s.customerRepo.UpdateInfo(info); err != nil {
+		return err
+	}
+
+	return nil
+}
