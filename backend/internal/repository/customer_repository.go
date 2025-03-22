476b775bab793d85ff73e8b4fc405df29b1acc26
nikita <nikitashumov@gmail.com>
2025-03-22 14:14:43 +0300

    refactor(backend/customer_repository): add detailed return error

--- backend/internal/repository/customer_repository.go
+++ backend/internal/repository/customer_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"invacc-backend/internal/models"
 
 	"gorm.io/gorm"
@@ -23,14 +24,15 @@ func (r *customerRepo) GetInfo(id uint) (models.CustomerInfo, error) {
 	var info models.CustomerInfo
 	result := r.db.Where("id = ?", id).First(&info)
 	if result.Error != nil {
-		return models.CustomerInfo{}, result.Error
+		return models.CustomerInfo{}, fmt.Errorf("couldn't get customer info by id %d: %w", id, result.Error)
 	}
 	return info, nil
 }
 
 func (r *customerRepo) UpdateInfo(info models.CustomerInfo) error {
-	if err := r.db.Save(&info).Error; err != nil {
-		return err
+	result := r.db.Save(&info)
+	if result.Error != nil {
+		return fmt.Errorf("couldn't update info by id %d: %w", info.ID, result.Error)
 	}
 	return nil
 }
