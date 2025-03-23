353db700f58d94f18093038ed77423facb7fe708
nikita <nikitashumov@gmail.com>
2025-03-23 21:56:51 +0300

    refactor(backend/models): update fields Item and CustomerInfo

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -15,6 +15,7 @@ type Item struct {
 	Price           float64        `gorm:"type:decimal(10,2);not null" json:"price"`
 	LatePenalty     float64        `gorm:"type:decimal(10,2);not null" json:"late_penalty"`
 	Active          bool           `gorm:"not null;default:true" json:"active"`
+	Img_url         string         `gorm:"type:text;not null" json:"img_url"`
 }
 
 func (Item) TableName() string {
@@ -34,13 +35,15 @@ func (CustomerAuth) TableName() string {
 
 // CustomersInfo in db
 type CustomerInfo struct {
-	ID        uint   `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	FirstName string `gorm:"column:firstname;type:varchar(50);not null" json:"firstname"`
-	LastName  string `gorm:"column:lastname;type:varchar(50);not null" json:"lastname"`
-	Phone     string `gorm:"column:phone;type:varchar(30);not null;unique" json:"phone"`
-	Email     string `gorm:"column:email;type:varchar(50);not null;default:'empty'" json:"email"`
-	Address   string `gorm:"column:address;type:varchar(100);not null;default:'empty'" json:"address"`
-	Passport  string `gorm:"column:passport;type:varchar(30);not null;default:'empty'" json:"passport"`
+	ID                uint   `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	FirstName         string `gorm:"column:firstname;type:varchar(50);not null" json:"firstname"`
+	LastName          string `gorm:"column:lastname;type:varchar(50);not null" json:"lastname"`
+	Phone             string `gorm:"column:phone;type:varchar(30);not null;unique" json:"phone"`
+	Phone_verified    bool   `gorm:"column:phone_verified;type:boolean;not null;default:false" json:"phone_verified"`
+	Email             string `gorm:"column:email;type:varchar(50);not null;default:'empty'" json:"email"`
+	Email_verified    bool   `gorm:"column:email_verified;type:boolean;not null;default:false" json:"email_verified"`
+	Passport          string `gorm:"column:passport;type:varchar(30);not null;default:'empty'" json:"passport"`
+	Passport_verified bool   `gorm:"column:passport_verified;type:boolean;not null;default:false" json:"passport_verified"`
 }
 
 func (CustomerInfo) TableName() string {
