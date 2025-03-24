6fbe90f21002e4da828c6f8a09f32a037bc2cbb5
nikita <nikitashumov@gmail.com>
2025-03-24 22:32:19 +0300

    refactor(sql): add comments

--- base/createDB/07_extensions.sql
+++ base/createDB/07_extensions.sql
@@ -1,3 +1,7 @@
 -- TODO Add functions pgAgent
 -- 1) update monthly_rent_summary (view)
+<<<<<<< HEAD
 -- 2) update overdue rents
+=======
+-- 2) update overdue rents
+>>>>>>> 3d65eca (refactor(sql): add comments)
