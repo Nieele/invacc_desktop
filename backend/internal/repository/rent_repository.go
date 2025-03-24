8c470c10b96604c7564f59b0a9f17a5d4489aac6
nikita <nikitashumov@gmail.com>
2025-03-24 11:59:52 +0300

    feat(backend/rent_repository): add get, add, remove in cart

--- backend/internal/repository/rent_repository.go
+++ backend/internal/repository/rent_repository.go
@@ -1,10 +1,15 @@
 package repository
 
 import (
+	"invacc-backend/internal/models"
+
 	"gorm.io/gorm"
 )
 
 type RentRepository interface {
+	getCart(CustomerID uint) ([]models.Cart, error)
+	addToCart(CustomerID uint, itemID uint) error
+	removeFromCart(CustomerID uint, itemID uint) error
 }
 
 type rentRepo struct {
@@ -13,4 +18,42 @@ type rentRepo struct {
 
 func NewRentRepository(db *gorm.DB) RentRepository {
 	return &rentRepo{db: db}
+}
+
+func (r *rentRepo) getCart(CustomerID uint) ([]models.Cart, error) {
+	var cart []models.Cart
+	result := r.db.Model(&models.Cart{}).
+		Where("user_id = ?", CustomerID).
+		Find(&cart)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return cart, nil
+}
+
+func (r *rentRepo) addToCart(CustomerID uint, itemID uint) error {
+	cart := models.Cart{
+		CustomerID: CustomerID,
+		ItemID:     itemID,
+	}
+	result := r.db.Create(&cart)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (r *rentRepo) removeFromCart(CustomerID uint, itemID uint) error {
+	result := r.db.Where("user_id = ? AND item_id = ?", CustomerID, itemID).
+		Delete(&models.Cart{})
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
 }
