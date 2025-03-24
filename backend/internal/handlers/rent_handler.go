6595068aaef0d456d316f39da72d18bed177c17c
nikita <nikitashumov@gmail.com>
2025-03-24 14:18:06 +0300

    feat(backend/handlers): add new handler - rent_handler

---
+++ backend/internal/handlers/rent_handler.go
@@ -0,0 +1,168 @@
+package handlers
+
+import (
+	"encoding/json"
+	"invacc-backend/internal/middleware"
+	"invacc-backend/internal/service"
+	"net/http"
+)
+
+type RentHandler interface {
+	GetCart(w http.ResponseWriter, r *http.Request)
+	AddToCart(w http.ResponseWriter, r *http.Request)
+	RemoveFromCart(w http.ResponseWriter, r *http.Request)
+
+	Rent(w http.ResponseWriter, r *http.Request)
+	CancelRent(w http.ResponseWriter, r *http.Request)
+}
+
+type rentHandler struct {
+	rentService service.RentService
+}
+
+func NewRentHandler(rentService service.RentService) RentHandler {
+	return &rentHandler{rentService: rentService}
+}
+
+// GET /rent/cart
+func (h *rentHandler) GetCart(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method is not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	userID, ok := r.Context().Value(middleware.UserCtxKey).(uint)
+	if !ok {
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	cartItems, err := h.rentService.GetCart(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cartItems)
+}
+
+// POST /cart. body: {"item_id": 123}
+func (h *rentHandler) AddToCart(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method is not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	userID, ok := r.Context().Value(middleware.UserCtxKey).(uint)
+	if !ok {
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var payload struct {
+		ItemID uint `json:"item_id"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, "incorrect format data", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.rentService.AddToCart(userID, payload.ItemID); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "item added to cart"})
+}
+
+// DELETE /cart. body: {"item_id": 123}
+func (h *rentHandler) RemoveFromCart(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "method is not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	userID, ok := r.Context().Value(middleware.UserCtxKey).(uint)
+	if !ok {
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var payload struct {
+		ItemID uint `json:"item_id"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, "incorrect format data", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.rentService.RemoveFromCart(userID, payload.ItemID); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "item removed from cart"})
+}
+
+// POST /rent/confirm {"items_id": [101, 102, 103]}
+func (h *rentHandler) Rent(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method is not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	userID, ok := r.Context().Value(middleware.UserCtxKey).(uint)
+	if !ok {
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var payload struct {
+		ItemsID []uint `json:"items_id"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, "incorrect format data", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.rentService.Rent(userID, payload.ItemsID); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "rent confirmed"})
+}
+
+// DELETE /rent/cancel {"items_id": [101, 102]}
+func (h *rentHandler) CancelRent(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method is not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	userID, ok := r.Context().Value(middleware.UserCtxKey).(uint)
+	if !ok {
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var payload struct {
+		ItemsID []uint `json:"items_id"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, "incorrect format data", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.rentService.CancelRent(userID, payload.ItemsID); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "rent canceled"})
+}
