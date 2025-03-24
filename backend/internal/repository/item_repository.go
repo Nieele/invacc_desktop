099c7e5f4bd00f3e144cbb87acc200a2cde75be6
nikita <nikitashumov@gmail.com>
2025-03-24 11:16:54 +0300

    feat(backend/item_repository): add warehouse name in item struct

--- backend/internal/repository/item_repository.go
+++ backend/internal/repository/item_repository.go
@@ -8,8 +8,8 @@ import (
 )
 
 type ItemRepository interface {
-	GetItem(id uint) (models.Item, error)
-	GetItemsList(page uint) ([]models.Item, error)
+	GetItem(id uint) (models.ItemWithWarehouseName, error)
+	GetItemsList(page uint) ([]models.ItemWithWarehouseName, error)
 }
 
 type itemRepo struct {
@@ -20,22 +20,36 @@ func NewItemRepository(db *gorm.DB) ItemRepository {
 	return &itemRepo{db: db}
 }
 
-func (r *itemRepo) GetItem(id uint) (models.Item, error) {
-	var item models.Item
-	result := r.db.Where("id = ?", id).First(&item)
+func (r *itemRepo) GetItem(id uint) (models.ItemWithWarehouseName, error) {
+	var item models.ItemWithWarehouseName
+	result := r.db.Model(&models.Item{}).
+		Select("items.*, warehouses.name as warehouse_name").
+		Joins("JOIN warehouses ON items.warehouse_id = warehouses.id").
+		Where("items.id = ?", id).
+		First(&item)
+
 	if result.Error != nil {
-		return models.Item{}, fmt.Errorf("couldn't find the item with the id %d: %w", id, result.Error)
+		return models.ItemWithWarehouseName{}, fmt.Errorf("couldn't find the item with the id %d: %w", id, result.Error)
 	}
+
 	return item, nil
 }
 
-func (r *itemRepo) GetItemsList(page uint) ([]models.Item, error) {
+func (r *itemRepo) GetItemsList(page uint) ([]models.ItemWithWarehouseName, error) {
 	const limit = 20
 	offset := (page - 1) * limit
-	var items []models.Item
-	result := r.db.Order("id").Offset(int(offset)).Limit(limit).Find(&items)
+	var items []models.ItemWithWarehouseName
+	result := r.db.Model(&models.Item{}).
+		Select("items.*, warehouses.name as warehouse_name").
+		Joins("JOIN warehouses ON items.warehouse_id = warehouses.id").
+		Order("items.id").
+		Offset(int(offset)).
+		Limit(limit).
+		Find(&items)
+
 	if result.Error != nil {
 		return nil, fmt.Errorf("couldn't get list items for page %d: %w", page, result.Error)
 	}
+
 	return items, nil
 }
