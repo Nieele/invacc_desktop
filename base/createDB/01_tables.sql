25d75fa9b4e75a32cb4d6117e6df7b4c7116eb0c
nikita <nikitashumov@gmail.com>
2025-04-13 18:34:01 +0300

    feat(sql): add deposit to items

--- base/createDB/01_tables.sql
+++ base/createDB/01_tables.sql
@@ -56,6 +56,7 @@ CREATE TABLE IF NOT EXISTS Items (
     quality          int            NOT NULL  DEFAULT 100  CHECK (quality >= 0 AND quality <= 100),
     price            decimal(10,2)  NOT NULL               CHECK (price > 0),
     late_penalty     decimal(10,2)  NOT NULL               CHECK (late_penalty > 0),
+    deposit          decimal(10,2)  NOT NULL               CHECK (deposit >= 0),
     active           boolean        NOT NULL  DEFAULT TRUE,
     img_url          text           NULL,
     CONSTRAINT fk_items_warehouses FOREIGN KEY (warehouse_id) REFERENCES Warehouses (id) ON DELETE RESTRICT ON UPDATE CASCADE
