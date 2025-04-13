646df72b65b280fa89bb23e44aabaa7740ff04d2
nikita <nikitashumov@gmail.com>
2025-04-13 20:09:40 +0300

    refactor(sql/roles): update roles permissions

--- base/createDB/06_roles.sql
+++ base/createDB/06_roles.sql
@@ -22,13 +22,10 @@ $$ LANGUAGE plpgsql;
 -- Create all roles
 SELECT create_role_if_not_exists('postgres');
 SELECT create_role_if_not_exists('register');
+SELECT create_role_if_not_exists('admin');
 SELECT create_role_if_not_exists('unknown');
 SELECT create_role_if_not_exists('worker');
 SELECT create_role_if_not_exists('inventory_manager');
-SELECT create_role_if_not_exists('marketing_specialist');
-SELECT create_role_if_not_exists('director');
-SELECT create_role_if_not_exists('moderator');
-SELECT create_role_if_not_exists('admin');
 
 
 --------------------------------------------------
@@ -41,17 +38,28 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 ALTER ROLE register WITH NOSUPERUSER NOINHERIT CREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'password';
 ALTER ROLE unknown WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
 
+-- Admin role configuration
+DO $$
+BEGIN
+    ALTER ROLE admin WITH SUPERUSER CREATEDB CREATEROLE REPLICATION BYPASSRLS NOLOGIN;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
+    GRANT ALL PRIVILEGES ON SCHEMA public TO admin;
+END $$;
+
 -- Worker role configuration
 DO $$
 BEGIN
     ALTER ROLE worker WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
     
     -- Worker permissions
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Rent, CustomersInfo TO worker;
-    GRANT SELECT, INSERT ON TABLE RentHistory TO worker;
-    GRANT USAGE, SELECT ON SEQUENCE rent_id_seq, customersinfo_id_seq, renthistory_id_seq TO worker;
-    GRANT SELECT ON TABLE Items, ItemsCategories, Categories, ItemsDiscounts, Discounts, 
-                          WarehousesOrders, ItemsDecommissioning TO worker;
+    GRANT SELECT, UPDATE, DELETE ON TABLE Rent                                      TO worker;
+    GRANT SELECT, UPDATE         ON TABLE CustomersInfo                             TO worker;
+    GRANT SELECT, INSERT         ON TABLE RentHistory                               TO worker;
+    GRANT SELECT                 ON TABLE CustomersAuth, DeliveryStatus, Warehouses,
+        WarehousesOrders, Employees, Employees_roles, ItemsDecommissioning          TO worker;
+
+    GRANT USAGE, SELECT ON SEQUENCE renthistory_id_seq TO worker;
 END $$;
 
 -- Inventory Manager role configuration
@@ -60,51 +68,14 @@ BEGIN
     ALTER ROLE inventory_manager WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
     
     -- Inventory Manager permissions
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ItemsDecommissioning, WarehousesOrders, 
-                                              ItemsCategories, Categories TO inventory_manager;
-    GRANT SELECT, INSERT, UPDATE ON TABLE WarehousesOrders, Items, ItemsServiceHistory TO inventory_manager;
-    GRANT USAGE, SELECT ON SEQUENCE itemsdecommissioning_id_seq, warehousesorders_id_seq, 
-                                    categories_id_seq, items_id_seq, itemsservicehistory_id_seq TO inventory_manager;
-    GRANT SELECT ON TABLE Employees, Employees_roles, Warehouses, RentHistory, Rent TO inventory_manager;
-END $$;
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ItemsDecommissioning                                               TO inventory_manager;
+    GRANT SELECT, INSERT, UPDATE         ON TABLE Items, ItemsDecommissioning, ItemsServiceHistory, WarehousesOrders TO inventory_manager;
+    GRANT SELECT, INSERT, UPDATE         ON TABLE Categories, ItemsCategories                                        TO inventory_manager;
+    GRANT SELECT                         ON TABLE Warehouses, RentHistory, Employees, Employees_roles                TO inventory_manager;
 
--- Marketing Specialist role configuration
-DO $$
-BEGIN
-    ALTER ROLE marketing_specialist WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
-    
-    -- Marketing Specialist permissions
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE ItemsDiscounts, Discounts TO marketing_specialist;
-    GRANT USAGE, SELECT ON SEQUENCE discounts_id_seq TO marketing_specialist;
-    GRANT SELECT ON TABLE Items, ItemsCategories, Categories, ItemsServiceHistory, Warehouses, 
-                          WarehousesOrders, ItemsDecommissioning, Rent, RentHistory TO marketing_specialist;
-END $$;
-
--- Director role configuration
-DO $$
-BEGIN
-    ALTER ROLE director WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
-    
-    -- Director permissions
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Warehouses, Employees TO director;
-    GRANT USAGE, SELECT ON SEQUENCE warehouses_id_seq, employees_id_seq TO director;
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO director;
-END $$;
-
--- Moderator role configuration
-DO $$
-BEGIN
-    ALTER ROLE moderator WITH NOSUPERUSER INHERIT CREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
-    GRANT unknown, worker, inventory_manager, marketing_specialist TO moderator WITH ADMIN OPTION;
-END $$;
-
--- Admin role configuration
-DO $$
-BEGIN
-    ALTER ROLE admin WITH SUPERUSER CREATEDB CREATEROLE REPLICATION BYPASSRLS NOLOGIN;
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
-    GRANT ALL PRIVILEGES ON SCHEMA public TO admin;
+    GRANT USAGE, SELECT ON SEQUENCE itemsdecommissioning_id_seq, items_id_seq, 
+        itemsservicehistory_id_seq, warehousesorders_id_seq, categories_id_seq, 
+        itemscategories_id_seq TO inventory_manager;
 END $$;
 
 COMMIT;
