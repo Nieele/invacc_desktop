9aecc04a245d84fa743bdb709684dbf79a83fac5
nikita <nikitashumov@gmail.com>
2025-04-17 03:12:16 +0300

    feat(backend/item_repository): add filters

--- backend/internal/repository/item_repository.go
+++ backend/internal/repository/item_repository.go
@@ -1,15 +1,26 @@
 package repository
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"invacc-backend/internal/models"
 
 	"gorm.io/gorm"
 )
 
+var ErrItemNotFound = errors.New("item not found")
+
+// query filters
+type ItemFilter struct {
+	CategoryID  *uint
+	WarehouseID *uint
+	Name        *string
+}
+
 type ItemRepository interface {
-	GetItem(id uint) (models.ItemWithWarehouseName, error)
-	GetItemsList(page uint) ([]models.ItemWithWarehouseName, error)
+	ListItems(ctx context.Context, filter *ItemFilter, offset, limit int) ([]models.Item, int64, error)
+	GetItemByID(ctx context.Context, id uint) (models.Item, error)
 }
 
 type itemRepo struct {
@@ -20,36 +31,62 @@ func NewItemRepository(db *gorm.DB) ItemRepository {
 	return &itemRepo{db: db}
 }
 
-func (r *itemRepo) GetItem(id uint) (models.ItemWithWarehouseName, error) {
-	var item models.ItemWithWarehouseName
-	result := r.db.Model(&models.Item{}).
-		Select("items.*, warehouses.name as warehouse_name").
-		Joins("JOIN warehouses ON items.warehouse_id = warehouses.id").
-		Where("items.id = ?", id).
-		First(&item)
+func (r *itemRepo) ListItems(ctx context.Context, filter *ItemFilter, offset, limit int) ([]models.Item, int64, error) {
+	// prepare the query
+	var items []models.Item
+	q := r.db.WithContext(ctx).Model(&models.Item{}).
+		Preload("Warehouse").
+		Preload("Categories").
+		Order("items.active DESC")
 
-	if result.Error != nil {
-		return models.ItemWithWarehouseName{}, fmt.Errorf("couldn't find the item with the id %d: %w", id, result.Error)
+	// category filter
+	if filter.CategoryID != nil {
+		// TODO: use gorm's many2many relation?
+		q = q.Joins("JOIN itemscategories ic ON ic.item_id = items.id").
+			Where("ic.category_id = ?", *filter.CategoryID)
 	}
 
-	return item, nil
+	// warehouse filter
+	if filter.WarehouseID != nil {
+		q = q.Where("items.warehouse_id = ?", *filter.WarehouseID)
+	}
+
+	// name filter
+	if filter.Name != nil {
+		q = q.Where("items.name ILIKE ?", "%"+*filter.Name+"%")
+	}
+
+	// execute the query
+
+	// count total items
+	var total int64
+	if err := q.Count(&total).Error; err != nil {
+		return nil, 0, fmt.Errorf("count items: %w", err)
+	}
+
+	// fetch items
+	if err := q.Offset(offset).Limit(limit).Find(&items).Error; err != nil {
+		return nil, 0, fmt.Errorf("list items: %w", err)
+	}
+
+	return items, total, nil
 }
 
-func (r *itemRepo) GetItemsList(page uint) ([]models.ItemWithWarehouseName, error) {
-	const limit = 20
-	offset := (page - 1) * limit
-	var items []models.ItemWithWarehouseName
-	result := r.db.Model(&models.Item{}).
-		Select("items.*, warehouses.name as warehouse_name").
-		Joins("JOIN warehouses ON items.warehouse_id = warehouses.id").
-		Order("items.active DESC").
-		Offset(int(offset)).
-		Limit(limit).
-		Find(&items)
+func (r *itemRepo) GetItemByID(ctx context.Context, id uint) (models.Item, error) {
+	// execute the query
+	var it models.Item
+	err := r.db.Model(&models.Item{}).
+		Preload("Warehouse").
+		Preload("Categories").
+		First(&it, id).
+		Error
 
-	if result.Error != nil {
-		return nil, fmt.Errorf("couldn't get list items for page %d: %w", page, result.Error)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return models.Item{}, ErrItemNotFound
+		}
+		return models.Item{}, fmt.Errorf("get item: %w", err)
 	}
 
-	return items, nil
+	return it, nil
 }
